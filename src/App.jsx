d20ac943c54fae1ddc883e718a47eb4031f5b153
airobus <923828430@qq.com>
2025-05-25 20:36:01 +0800

    Convert to Vite React Project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react';
+import { colorPalette } from './constants';
+import { useGameLoop } from './hooks/useGameLoop';
+import './App.css';
+
+function App() {
+  const canvasRef = useRef(null);
+  const scoreRef = useRef(null);
+  const { dayScore, nightScore } = useGameLoop(canvasRef, scoreRef);
+
+  return (
+    <div id="container">
+      <canvas ref={canvasRef} id="pongCanvas" width="600" height="600" />
+      <div ref={scoreRef} id="score">day {dayScore} | night {nightScore}</div>
+      <p id="made">
+        made by
+        <a href="https://koenvangilst.nl/labs/pong-wars">Koen van Gilst</a> | source on
+        <a href="https://github.com/vnglst/pong-wars">github</a>
+      </p>
+    </div>
+  );
+}
+
+export default App
